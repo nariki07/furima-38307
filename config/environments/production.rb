947ed6318612013773d5d958de472256780df701
nariki07 <110153985+nariki07@users.noreply.github.com>
2022-09-01 18:46:13 +0900

    自動デプロイの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,3 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+server '35.74.57.83', user: 'ec2-user', roles: %w{app db web}
