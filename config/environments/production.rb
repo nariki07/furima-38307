e50f88ab6ccdb2cf316de5d260c3b15843781291
nariki07 <110153985+nariki07@users.noreply.github.com>
2022-09-01 20:47:08 +0900

    capsfileの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-end
+  server '35.74.57.83', user: 'ec2-user', roles: %w{app db web}
 
-server '35.74.57.83', user: 'ec2-user', roles: %w{app db web}
+end
