cd6b567663698dfdb44e2d01dd80156cd08b7b8f
nariki07 <110153985+nariki07@users.noreply.github.com>
2022-09-01 21:29:22 +0900

    テスト

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :application, 'furima-38307'
 set :repo_url,  'git@github.com:nariki07/furima-38307.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
